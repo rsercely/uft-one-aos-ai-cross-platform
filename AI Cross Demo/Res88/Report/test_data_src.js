6fceca56eeb28f435585d659b7c28090e2692827
demo <admin@default.com>
2020-10-22 15:34:35 -0500

    removes all the Res directories

--- AI Cross Demo/Res88/Report/test_data_src.js
+++
@@ -1,1 +0,0 @@
-﻿var __testDataSource={items:[{dataFilePath:'Default.xlsx',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\demo\\Documents\\Unified Functional Testing\\uft-one-aos-ai-cross-platform\\AI Cross Demo\\Res88',namePairs:[]}]};
